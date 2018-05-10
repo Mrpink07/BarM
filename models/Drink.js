4bb1291582e8ac6d81a0dc6b9063b70c64d4e3a5
Markus <markus.iversen@gmail.com>
2018-05-10 21:56:31 +0000

    Changed drinks DB model to include measurements and delay

--- models/Drink.js
+++ models/Drink.js
@@ -4,8 +4,10 @@ exports.DrinkSchema = new Mongoose.Schema({
   name: { type: String, required: true },
   besk: { type: String, required: false },
   image: { type: String, required: false },
+  measurement: { type: String, required: false},
   ingredients: [{
     name: String,
-    amount: Number
+    amount: Number,
+    delay: Number
   }]
 });
