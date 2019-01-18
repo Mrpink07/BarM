2cf7bc1b5d75175a74af2cb2bf19f1d77bcffcaa
Markus <markus.iversen@gmail.com>
2019-01-18 18:50:08 +0000

    Added test HTTPS request for future API use

--- routes/drinkshistory.js
+++ routes/drinkshistory.js
@@ -1,3 +1,5 @@
+var https = require('https');
+const querystring = require('querystring');                                                                                                                                                                                                
 
 // Add to history collection
 exports.add = function (History) {
@@ -9,6 +11,37 @@ exports.add = function (History) {
       if (err || !history) {
         res.json({ error: err });
       } else {
+        // Send the details off to the remote server
+        var postData = querystring.stringify({
+            'test': 'Test'
+        });
+        
+        console.log("Contacting API...");
+        console.log(history);
+        
+        var options = {
+          hostname: 'barmachina.free.beeceptor.com',
+          port: 443,
+          path: '/post',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': postData.length
+          }
+        };
+        
+        // Send the request
+        var httpsReq = https.request(options, (httpsRes) => {
+          console.log('statusCode:', httpsRes.statusCode);
+          console.log('headers:', httpsRes.headers);
+        });
+        
+        // Handle any errors for the request
+        httpsReq.on('error', (e) => {
+          console.log('Unable to reach API');
+          console.error(e);
+        });
+
         res.json({ history: history });
       }
     });
