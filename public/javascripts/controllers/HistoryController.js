fa2f95ecbaabb2390650ff9410312ebcb2f844a3
Markus <markus.iversen@gmail.com>
2019-01-18 18:19:58 +0000

    Fixed bug with history date inputs and add link to home page

--- public/javascripts/controllers/HistoryController.js
+++ public/javascripts/controllers/HistoryController.js
@@ -10,6 +10,10 @@ function HistoryController($scope, $http) {
     // Set tomorrow's date
     $scope.tomorrow = new Date();
     $scope.tomorrow.setDate($scope.tomorrow.getDate() + 1);
+    
+    // Set the from date
+    $scope.fromDate = [{day: "01"}, {month: "01"}, {year: "2019"}];
+    $scope.fromTo = [{day: "20"}, {month: "01"}, {year: "2019"}];
   };
   
 
